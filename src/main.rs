95b91910be7a917cf8468ea1cd3d3b36d3d417d3
conblem <mail@conblem.me>
2021-03-10 15:56:18 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,10 @@
 use std::error::Error;
-use std::io::Read;
-
-use tokio;
-use tokio::time::delay_for;
 
 mod socks5;
 use socks5::Socks5Listener;
 
-mod race;
-use race::race;
-
-use rustls::internal::msgs::enums::NamedGroup;
-use rustls::internal::msgs::codec::Reader;
-use rustls::internal::msgs::codec::Codec;
-use rustls::internal::msgs::handshake::KeyShareEntries;
-use rustls::internal::msgs::enums::CipherSuite;
-
-use ring::digest::digest;
-use ring::digest::SHA256;
-
-use byteorder::{ReadBytesExt, BigEndian};
-
-use base64;
-
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    let esni = "/wHndGGZACQAHQAgNvYgGft2eIBZ33NG8uc9+UFtmSTA/kK+ZdTvleiZ01sAAhMBAQQAAAAAXnBnYAAAAABeeFBgAAA=";
-    let esni = base64::decode(esni).expect("invalid base64");
-    let mut esni = &esni[..];
-
-    let version = esni.read_u16::<BigEndian>()?;
-
-    let mut checksum = [0; 4];
-    esni.read_exact(&mut checksum)?;
-    let calculated_checksum = digest(&SHA256, esni);
-
-    let mut keyshare_entry_size = [0; 2];
-    keyshare_entry_size.copy_from_slice(&esni[..2]);
-    let keyshare_entry_size = u16::from_be_bytes(keyshare_entry_size) as usize;
-
-    let mut keyshare_entry = vec![0; keyshare_entry_size + 2];
-    esni.read_exact(&mut keyshare_entry);
-    let mut reader = Reader::init(&keyshare_entry);
-    let keyshare_entries = KeyShareEntries::read(&mut reader).expect("keyshare");
-
-    let ciphersuite_size = esni.read_u16::<BigEndian>()? as usize;
-    let mut ciphersuite = vec![0; ciphersuite_size];
-    esni.read_exact(&mut ciphersuite);
-    let mut reader = Reader::init(&ciphersuite);
-    let ciphersuite = CipherSuite::read(&mut reader).expect("ciphersuite");
-
-    let padded_lenght = esni.read_u16::<BigEndian>()?;
-    let not_before = esni.read_u64::<BigEndian>()?;
-    let not_after = esni.read_u64::<BigEndian>()?;
-
-    let extension_size = esni.read_u16::<BigEndian>()?;
-
-    println!("start");
     let mut socks5 = Socks5Listener::new().await?;
     socks5.listen().await?;
     Ok(())
--- src/socks5.rs
+++ src/socks5.rs
@@ -1,17 +1,17 @@
-use std::str;
-use std::sync::Arc;
 use std::error::Error;
-use std::collections::HashMap;
-use std::net::{SocketAddrV4, Ipv4Addr};
+use std::net::{Ipv4Addr, SocketAddrV4};
+use std::str;
 
 use tokio;
-use tokio::join;
-use tokio::sync::{Mutex, RwLock};
-use tokio::net::{TcpListener, TcpStream};
 use tokio::io::{AsyncReadExt, AsyncWriteExt, Error as IOError, ErrorKind};
+use tokio::join;
+use tokio::net::{TcpListener, TcpStream};
 
-use trust_dns_resolver::TokioAsyncResolver;
+use std::ops::AddAssign;
+use tokio::sync::mpsc::{unbounded_channel, UnboundedSender};
+use tokio::task::JoinHandle;
 use trust_dns_resolver::config::{ResolverConfig, ResolverOpts};
+use trust_dns_resolver::TokioAsyncResolver;
 
 #[allow(dead_code)]
 mod v5 {
@@ -34,14 +34,12 @@ mod v5 {
     pub const REPLY_TTL_EXPIRED: u8 = 0x06;
     pub const REPLY_CMD_UNSUPPORTED: u8 = 0x07;
     pub const REPLY_ATYP_UNSUPPORTED: u8 = 0x08;
-
 }
 
 struct Socks5Stream {
     socket: TcpStream,
     resolver: TokioAsyncResolver,
-    domains: Arc<RwLock<HashMap<String, ()>>>,
-    counter: Arc<Mutex<u8>>
+    sender: UnboundedSender<ConnectionCount>,
 }
 
 impl Socks5Stream {
@@ -81,16 +79,26 @@ impl Socks5Stream {
         Ok(socket.read_u8().await?)
     }
 
-    async fn resolve_ipv4(socket: &mut TcpStream) -> Result<Ipv4Addr, Box<dyn Error + Send + Sync>> {
+    async fn resolve_ipv4(
+        socket: &mut TcpStream,
+    ) -> Result<Ipv4Addr, Box<dyn Error + Send + Sync>> {
         let mut dst_addr = [0x00; 4];
         socket.read_exact(&mut dst_addr).await?;
-        Ok(Ipv4Addr::new(dst_addr[0], dst_addr[1], dst_addr[2], dst_addr[3]))
+        Ok(Ipv4Addr::new(
+            dst_addr[0],
+            dst_addr[1],
+            dst_addr[2],
+            dst_addr[3],
+        ))
     }
 
-    async fn resolve_domainame(socket: &mut TcpStream, resolver: &TokioAsyncResolver) -> Result<Ipv4Addr, Box<dyn Error + Send + Sync>> {
+    async fn resolve_domainame(
+        socket: &mut TcpStream,
+        resolver: &TokioAsyncResolver,
+    ) -> Result<Ipv4Addr, Box<dyn Error + Send + Sync>> {
         let fqdn_size = socket.read_u8().await? as usize;
         if fqdn_size > 255 {
-            return Err(Box::new(IOError::new(ErrorKind::Other, "domain too long")))
+            return Err(Box::new(IOError::new(ErrorKind::Other, "domain too long")));
         }
 
         let mut buf: Vec<u8> = vec![0x00; fqdn_size];
@@ -106,22 +114,27 @@ impl Socks5Stream {
         }
     }
 
-    async fn resolve_socket(socket: &mut TcpStream, dst_addr: Ipv4Addr) -> Result<TcpStream, Box<dyn Error + Send + Sync>> {
+    async fn resolve_socket(
+        socket: &mut TcpStream,
+        dst_addr: Ipv4Addr,
+    ) -> Result<TcpStream, Box<dyn Error + Send + Sync>> {
         let dst_port = socket.read_u16().await?;
 
-        let dst = SocketAddrV4::new(
-            dst_addr,
-            dst_port
-        );
+        let dst = SocketAddrV4::new(dst_addr, dst_port);
 
         let stream = TcpStream::connect(dst).await?;
 
-        socket.write_all(&[0x05, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]).await?;
+        socket
+            .write_all(&[0x05, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+            .await?;
 
         Ok(stream)
     }
 
-    async fn pipe(socket: &mut TcpStream, mut stream: TcpStream) -> Result<(), Box<dyn Error + Send + Sync>> {
+    async fn pipe(
+        socket: &mut TcpStream,
+        mut stream: TcpStream,
+    ) -> Result<(), Box<dyn Error + Send + Sync>> {
         let (mut socket_read, mut socket_write) = socket.split();
         let (mut stream_read, mut stream_write) = stream.split();
 
@@ -131,11 +144,9 @@ impl Socks5Stream {
                 match socket_read.read(buffer.as_mut()).await {
                     Err(_) => break,
                     Ok(size) if size == 0 => break,
-                    Ok(size) => stream_write.write_all(&buffer[0..size]).await
+                    Ok(size) => stream_write.write_all(&buffer[0..size]).await,
                 };
             }
-            // add error handling
-            AsyncWriteExt::shutdown(&mut stream_write).await;
         };
 
         let from_stream = async move {
@@ -144,71 +155,72 @@ impl Socks5Stream {
                 match stream_read.read(buffer.as_mut()).await {
                     Err(_) => break,
                     Ok(size) if size == 0 => break,
-                    Ok(size) => socket_write.write_all(&buffer[0..size]).await
+                    Ok(size) => socket_write.write_all(&buffer[0..size]).await,
                 };
             }
-            // add error handling
-            AsyncWriteExt::shutdown(&mut socket_write).await;
         };
-
 
         join!(from_socket, from_stream);
         Ok(())
     }
 
-    async fn run(self: &mut Self) -> Result<(), Box<dyn Error + Send + Sync>> {
+    async fn run(mut self) -> Result<(), Box<dyn Error + Send + Sync>> {
         let socket = &mut self.socket;
         let resolver = &self.resolver;
-
-        let mut counter = self.counter.clone();
-        let mut counter_two = self.counter.clone();
-
-        let count_up = tokio::spawn(async move {
-            let mut counter = counter.lock().await;
-            *counter += 1;
-            println!("connection open {}", counter);
-        });
 
         Socks5Stream::resolve_method(socket).await?;
         let dst_addr = match Socks5Stream::resolve_atyp(socket).await? {
             v5::ATYP_IPV4 => Socks5Stream::resolve_ipv4(socket).await?,
             v5::ATYP_DOMAIN => Socks5Stream::resolve_domainame(socket, resolver).await?,
-            _ => return Err(Box::new(IOError::new(ErrorKind::Other, "invalid atyp")))
+            _ => return Err(Box::new(IOError::new(ErrorKind::Other, "invalid atyp"))),
         };
         let stream = Socks5Stream::resolve_socket(socket, dst_addr).await?;
         Socks5Stream::pipe(socket, stream).await?;
-
-        let count_down = tokio::spawn(async move {
-            let mut counter = counter_two.lock().await;
-            *counter -= 1;
-            println!("connection closed {}", counter);
-        });
-
-        count_up.await?;
-        count_down.await?;
 
         Ok(())
     }
 }
 
-pub struct Socks5Listener { listener: TcpListener,
+pub struct Socks5Listener {
+    listener: TcpListener,
     resolver: TokioAsyncResolver,
-    domains: Arc<RwLock<HashMap<String, ()>>>,
-    counter: Arc<Mutex<u8>>
+    receiver: JoinHandle<()>,
+    sender: UnboundedSender<ConnectionCount>,
+}
+
+enum ConnectionCount {
+    Increase,
+    Decrease,
+}
+
+impl AddAssign<ConnectionCount> for u8 {
+    fn add_assign(&mut self, rhs: ConnectionCount) {
+        match rhs {
+            ConnectionCount::Increase => *self += 1,
+            ConnectionCount::Decrease => *self -= 1,
+        }
+    }
 }
 
 impl Socks5Listener {
     pub(crate) async fn new() -> Result<Self, Box<dyn Error + Send + Sync>> {
         let listener = TcpListener::bind("127.0.0.1:1080").await?;
-        let resolver = TokioAsyncResolver::tokio(ResolverConfig::cloudflare_tls(), ResolverOpts::default()).await?;
-        let domains: Arc<RwLock<HashMap<String, ()>>> = Arc::new(RwLock::new(HashMap::new()));
-        let counter = Arc::new(Mutex::new(0));
+        let resolver =
+            TokioAsyncResolver::tokio(ResolverConfig::cloudflare_tls(), ResolverOpts::default())?;
+        let (sender, mut receiver) = unbounded_channel();
+        let receiver = tokio::spawn(async move {
+            let mut count = 0;
+            while let Some(val) = receiver.recv().await {
+                count += val;
+                println!("Connections {}", count);
+            }
+        });
 
         Ok(Socks5Listener {
             listener,
             resolver,
-            domains,
-            counter
+            receiver,
+            sender,
         })
     }
 
@@ -216,18 +228,16 @@ impl Socks5Listener {
         loop {
             let (socket, _) = self.listener.accept().await?;
             let resolver = self.resolver.clone();
-            let domains = self.domains.clone();
-            let counter = self.counter.clone();
+            let sender = self.sender.clone();
 
-            tokio::spawn(async move {
-                let mut stream = Socks5Stream {
+            tokio::spawn(
+                Socks5Stream {
                     socket,
                     resolver,
-                    domains,
-                    counter
-                };
-                stream.run().await;
-            });
+                    sender,
+                }
+                .run(),
+            );
         }
     }
 }
